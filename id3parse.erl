f371d0e54b0ec95471a661a8cbc1e03ccf7203cc
Hans Ulrich Niedermann <hun@n-dimensional.de>
2009-06-13 01:40:40 +0200

    Actually do basic ID3v2 parsing
    Fails on APIC still.

--- id3parse.erl
+++ id3parse.erl
@@ -1,4 +1,3 @@
-%%
 %% http://www.id3.org/id3v2.4.0-structure
 
 -module(id3parse).
@@ -9,10 +8,13 @@
 parse_data(Data, Acc) ->
     parse_header(Data, Acc).
 
+
+
 parse_header(<<
 	      "ID3",
 	      VerMajor, VerMinor,
 	      Flags:1/binary,
+	      Size:4/binary,
 	      Rest/binary
 	      >>,
 	     Acc) ->
@@ -26,34 +28,181 @@ parse_header(<<
      0:1,
      0:1
      >> = Flags,
+    {Unsync, FlagList0} = case FlagUnsync of
+			      1 -> {true,  [unsync]};
+			      0 -> {false, []}
+			  end,
+    FlagList1 = case FlagExtendedHeader of
+		    1 -> [ext_hdr|FlagList0];
+		    0 -> FlagList0
+		end,
+    FlagList2 = case FlagExperimental of
+		    1 -> [experimental|FlagList1];
+		    0 -> FlagList1
+		end,
+    FlagList = case FlagFooter of
+		   1 -> [footer|FlagList2];
+		   0 -> FlagList2
+	       end,
+    RealSize = unsynch(Unsync, Size),
     io:format("ID3 header: "
 	      "ver:id3v2.~w.~w "
-	      "flags:~w ( ~s~s~s~s)"
+	      "flags:~w (~w) "
+	      "size:~w"
 	      "~n",
 	      [
 	       VerMajor, VerMinor,
-	       Flags,
-	       case FlagUnsync of
-		   1 -> "unsync ";
-		   0 -> ""
-	       end,
-	       case FlagExtendedHeader of
-		   1 -> "exthdr ";
-		   0 -> ""
-	       end,
-	       case FlagExperimental of
-		   1 -> "exphdr ";
-		   0 -> ""
-	       end,
-	       case FlagFooter of
-		   1 -> "footer ";
-		   0 -> ""
-	       end
+	       Flags, FlagList,
+	       RealSize
 	      ]),
-    Hdr = {header, {id3v2version, VerMajor, VerMinor}},
+    Hdr = {header, [{id3v2version, VerMajor, VerMinor},
+		    {flags, FlagList},
+		    {real_size, RealSize}]},
     R1 = case FlagExtendedHeader of
-	     1 -> parse_extended_header(Rest, [Hdr|Acc]);
-	     0 -> parse
+	     1 -> parse_extended_header(Unsync, Rest, [Hdr|Acc]);
+	     0 -> parse_frame(Unsync, Rest, [Hdr|Acc])
+	 end,
+    R1.
+
+
+parse_extended_header(Unsync,
+		      <<
+		       ExtHdrSize:32/integer,
+		       ExtHdrStuff:ExtHdrSize/binary,
+		       Rest/binary
+		       >>, Acc) ->
+    <<
+     FlagByteCount:8/integer,
+     ExtendedFlags:FlagByteCount/binary
+     >> = ExtHdrStuff,
+    parse_frame(Unsync,
+		Rest,
+		[{extended_header,
+		  ExtHdrSize, FlagByteCount,
+		  ExtendedFlags}|Acc]).
+
+
+unsynch(true,
+	  <<
+	   0:1, S3:7,
+	   0:1, S2:7,
+	   0:1, S1:7,
+	   0:1, S0:7
+	   >>) ->
+    (((((S3 bsl 7) + S2) bsl 7) + S1) bsl 7) + S0;
+unsynch(false, <<S:32/integer>>) ->
+    S.
+
+
+parse_frame(Unsync,
+	    <<0,0,0,0, Rest/binary>>, Acc) ->
+    parse_frame(Unsync, Rest, Acc);
+parse_frame(Unsync,
+	    <<
+	     FrameID:4/binary,
+	     FrameSize:4/binary,
+	     FrameFlags:2/binary,
+	     Rest/binary
+	     >>, Acc) ->
+    {FirstFewBytes, _} = split_binary(Rest, 40),
+    RealSize = unsynch(Unsync, FrameSize),
+    io:format("frame id    (raw): ~w (~s)~n"
+	      "frame size  (raw): ~w (~w)~n"
+	      "frame flags (raw): ~w~n"
+	      "frame data  (raw): ~w...~n",
+	      [FrameID, binary_to_list(FrameID),
+	       FrameSize, RealSize,
+	       FrameFlags,
+	       FirstFewBytes]),
+    <<
+     0:1,
+     FrameTagAlterDiscard:1,
+     FrameFileAlterDiscard:1,
+     FrameReadOnly:1,
+
+     0:1, 0:1, 0:1, 0:1,
+
+     0:1,
+     FrameGroupingIdentity:1,
+     0:1,
+     0:1,
+
+     FrameCompressed:1,
+     FrameEncrypted:1,
+     FrameUnsync:1,
+     FrameDataLengthIndicator:1
+
+     >> = FrameFlags,
+
+    io:format("parse_frame id=\"~s\" size=~w=~w flags=~w~n",
+	      [binary_to_list(FrameID), FrameSize,
+	       RealSize, FrameFlags]),
+
+    <<
+     Data:RealSize/binary,
+     NewRest/binary
+     >> = Rest,
+
+    parse_frame_int(Unsync, FrameID, RealSize, FrameFlags, Data, NewRest, Acc).
+
+
+text_content_int(<<>>, Acc) ->
+    {lists:reverse(Acc), <<>>};
+text_content_int(<<0, Rest/binary>>, Acc) ->
+    {lists:reverse(Acc), Rest};
+text_content_int(<<Char, Rest/binary>>, Acc) ->
+    text_content_int(Rest, [Char|Acc]).
+
+text_content_int(Text) when is_binary(Text) ->
+    text_content_int(Text, []).
+
+
+text_content(<<>>, Acc) ->
+    lists:reverse(Acc);
+text_content(Text, Acc) ->
+    {String, NewText} = text_content_int(Text),
+    text_content(NewText, [String|Acc]).
+
+text_content(Text) when is_binary(Text) ->
+    text_content(Text, []).
+
+
+parse_frame_int(Unsync, <<"TXXX">> = FrameID, FrameSize, FrameFlags, Data, Rest, Acc) ->
+    parse_frame(Unsync, Rest, [{txxx_frame, FrameID, FrameSize, FrameFlags, Data}|Acc]);
+parse_frame_int(Unsync,
+		<<"T",T1:8/integer,T2:8/integer,T3:8/integer>> = FrameID,
+		FrameSize, FrameFlags,
+		<<TextEncoding:8/integer, Text/binary>> = Data,
+		Rest, Acc)
+  when ($0 =< T1), (T1 =< $Z),
+       ($0 =< T2), (T2 =< $Z),
+       ($0 =< T3), (T3 =< $Z)
+       ->
+    io:format("  Text tag: ~s~n"
+	      "  Encoding: ~w~n",
+	      [binary_to_list(FrameID),
+	       TextEncoding]),
+    io:format("   Content: ~p~n", [text_content(Text)]),
+    parse_frame(Unsync, Rest, [{text_frame, FrameID, FrameSize, FrameFlags, Data}|Acc]);
+parse_frame_int(Unsync,
+		<<T0:8/integer,T1:8/integer,T2:8/integer,T3:8/integer>>=FrameID,
+		FrameSize, FrameFlags, Data, Rest, Acc)
+  when ($0 =< T0), (T0 =< $Z),
+       ($0 =< T1), (T1 =< $Z),
+       ($0 =< T2), (T2 =< $Z),
+       ($0 =< T3), (T3 =< $Z)
+       ->
+    parse_frame(Unsync, Rest, [{generic_frame, FrameID, FrameSize, FrameFlags, Data}|Acc]).
+
+
+parse_footer(<<
+	      "3DI",
+	      VerMajor, VerMinor,
+	      Flags:1/binary,
+	      Rest/binary
+	      >>, Acc) ->
+    Acc.
+
 
 parse_data(Data) ->
     parse_data(Data, []).
@@ -61,12 +210,16 @@ parse_data(Data) ->
 
 test_item(FileName) ->
     {ok, Data} = file:read_file(FileName),
-    parse_data(Data).
+    P = parse_data(Data),
+    io:format("~s:~n  ~p~n", [FileName, P]),
+    P.
+
 
 test([], Acc) ->
     lists:reverse(Acc);
-test([Head|Tail], Acc) ->
-    test(Tail, [test_item(Head)|Acc]).
+test([FileName|Tail], Acc) ->
+    test(Tail, [test_item(FileName)|Acc]).
+
 
 test(List) when is_list(List) ->
     test(List, []).
